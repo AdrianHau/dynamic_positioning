8049e69d9711b481be24dbaeb3f93bef5bd3367d
Adrian Haugland <adhaugland@gmail.com>
2022-02-21 21:57:36 +0100

    initial implementation

--- regulator.py
+++ regulator.py
@@ -5,8 +5,7 @@ import time
 
 
 class PID:
-    """PID controller algorithm implementation. (Utdyp)
-    """
+    """PID controller algorithm implementation. (Utdyp)"""
 
     def __init__(self, kp, ki, kd, setpoint):
 
@@ -27,14 +26,14 @@ class PID:
         """Calculates thrust output with current error value. (Utdyp)"""
 
         current_time = time.time()
-        dt = (current_time - self.previous_time) if self.previous_time is not None else 0  # find a better fix
+        dt = current_time - self.previous_time if self.previous_time is not None else 0
         self.previous_time = current_time
 
         current_error = self.setpoint - current_pos
-        de = (current_error - self.previous_error)/2
+        de = (current_error - self.previous_error) / 2
 
         self.proportional_term = self.Kp * current_error
-        self.integral_term += self.Ki * (self.previous_error + current_error)/2 * dt
-        self.derivative_term = self.Kd * de/dt if dt is not 0 else 0  # find a better fix
+        self.integral_term += self.Ki * (self.previous_error + current_error) / 2 * dt
+        self.derivative_term = self.Kd * de / dt if dt is not 0 else 0
 
         return self.proportional_term + self.integral_term + self.derivative_term
